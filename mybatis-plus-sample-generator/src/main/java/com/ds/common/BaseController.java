af5cd3448fc9db196c1235e754cc3bfa9ba052c2
HEBO <13216192626@163.com>
2020-09-15 16:27:51 +0800

    customized

---
+++ mybatis-plus-sample-generator/src/main/java/com/ds/common/BaseController.java
@@ -0,0 +1,10 @@
+package com.ds.common;
+
+/**
+ * 基础controller
+ *
+ * @author dawn
+ */
+public class BaseController {
+
+}
---
+++ mybatis-plus-sample-generator/src/main/java/com/ds/common/BaseEntity.java
@@ -0,0 +1,15 @@
+package com.ds.common;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Base实体类
+ *
+ * @author dawn
+ */
+@Getter
+@Setter
+public class BaseEntity {
+
+}
